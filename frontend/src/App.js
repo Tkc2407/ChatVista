c38fa74c457c5a09e2892c4cbf6cebfd2f5ebe67
Tkc2407 <tapaschaubey@gmail.com>
2024-02-17 00:19:52 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Homepage from "./Pages/HomePage";
+import Chatpage from "./Pages/ChatPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage/>} />
+          {/* <Route exact path="/chats" element={<Chatpage/>} /> */}
+        </Routes>
+      </Router>
+    </div>
+
+  );
+}
+
+export default App;
