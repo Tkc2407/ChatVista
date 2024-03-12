e7f7d74df90ae24e7a32bcb7544cc5ed88a90f72
Tkc2407 <tapaschaubey@gmail.com>
2024-03-12 23:50:11 +0530

    created access chat api

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -69,8 +69,8 @@ const allUsers = asyncHandler(async (req, res) => {
             ],
         }
         : {};
-    const users = await User.find(keyword).find({ _id: {$ne:req.user._id }});
-    console.log(keyword);
+    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+    res.send(users);
 });
 
 module.exports = { registerUser, authUser, allUsers };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,8 +1,7 @@
-const express=require('express');
-const protect=require("..middleware/authMiddleware");
 const { protect } = require("../middleware/authMiddleware");
+const { accessChat } = require('../controllers/chatControllers');
 
-const router=express.Router();
+const router=(require('express')).Router();
 
 router.route("/").post(protect,accessChat);
 // router.route("/").get(protect,fetchChat);
