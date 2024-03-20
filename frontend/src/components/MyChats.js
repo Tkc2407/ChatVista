a68c08976b83c9233779db08d9b8e49ddd014dc7
Tkc2407 <tapaschaubey@gmail.com>
2024-03-21 03:38:38 +0530

    Added Chats

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useToast } from '@chakra-ui/react';
+import React, { useState } from 'react'
 
 const MyChats = () => {
+  const [loggedUser, setLoggedUser] = useState();
+
+  const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
+
+  const toast = useToast();
+
+  const fetchChats = async () => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer &{user.token}`;
+        },
+      };
+      const { data } = await axios.get("/api/chat", config);
+      setChats(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the chats",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
+  
   return (
     <div>
-      MyChats
+      const
     </div>
   )
 }
