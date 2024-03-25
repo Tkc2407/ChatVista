a9836f830c3730cccdc7cc3b8ba0d2354d54d40a
Tkc2407 <tapaschaubey@gmail.com>
2024-03-26 04:09:13 +0530

    Completed send message api

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,17 @@ const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json())
 
-// app.get("/",(req,res)=>{
-//     res.send("API is Running Successfully");
-// });
 
 app.use('/api/user',userRoutes);
 app.use('/api/chat',chatRoutes);
+app.use('/api/message',messageRoutes);
 
 app.listen(process.env.PORT || 80, () => {
     console.log("Backend server is running!");
