5435983969915a00499cb1e4002a757c79a644d7
Tkc2407 <tapaschaubey@gmail.com>
2024-03-26 02:08:23 +0530

    Completed group add feature

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.js
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -34,17 +34,129 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
     const { selectedChat, setSelectedChat, user } = ChatState();
 
     const handleSearch = async (query) => {
+        setSearch(query);
 
-    };
+        if (!query) {
+            return;
+        }
+        try {
+            setLoading(true);
+
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
+            };
+            const { data } = await axios.get(`/api/user?search=${search}`, config);
+            console.log(data);
+            setLoading(false);
+            setSearchResult(data);
+        }
+        catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: "Failed to Load the Search Results",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom-left",
+            });
+        }
+    }
 
     const handleRename = async () => {
+        if (!groupChatName) return;
+
+        try {
+            setRenameLoading(true);
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
+            };
+            const { data } = await axios.put(
+                `/api/chat/rename`,
+                {
+                    chatId: selectedChat._id,
+                    chatName: groupChatName,
+                },
+                config
+            );
+
+            console.log(data._id);
+            setSelectedChat(data);
+            setFetchAgain(!fetchAgain);
+            setRenameLoading(false);
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setRenameLoading(false);
+        }
+        setGroupChatName("");
     };
 
     const handleAddUser = async (user1) => {
+        if (selectedChat.users.find((u) => u._id === user1._id)) {
+            toast({
+                title: "User Already in group!",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            return;
+        }
+
+        if (selectedChat.groupAdmin._id !== user._id) {
+            toast({
+                title: "Only admins can add someone!",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            return;
+        }
+        try {
+            setLoading(true);
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
+            };
+            const { data } = await axios.put(
+                `/api/chat/groupadd`,
+                {
+                    chatId: selectedChat._id,
+                    userId: user1._id,
+                },
+                config
+            );
+
+            setSelectedChat(data);
+            setFetchAgain(!fetchAgain);
+            setLoading(false);
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+        }
+
     };
 
-    const handleRemove = async (user1) => {
-    };
+
 
     return (
         <>
@@ -100,7 +212,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
                         </FormControl>
 
                         {loading ? (
-                            <Spinner size="lg" />
+                            <Spinner size="sm" />
                         ) : (
                             searchResult?.map((user) => (
                                 <UserListItem
